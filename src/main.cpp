bf5050d1186d17d9d9cae7848196a133da39c970
notIsakS <notisaks@gmail.com>
2025-06-10 04:18:51 +0200

    added reboot for motor controller, sets everything back to default. Added descriptive texts.

--- src/main.cpp
+++ src/main.cpp
@@ -3,28 +3,39 @@
 #include <Arduino.h>
 
 
-/* States for which the rover can be in */
-enum State {
+// Statemachine
+enum State { 
   IDLE,
   MOVING
 };
 
-State currentState = IDLE;
+
+// Current state
+State currentState = IDLE; 
+
 
 void setup()
 {
-  Serial.begin(115200);
-  if (controller.begin())
-  {
+  //Serial port initialization
+  Serial.begin(115200); 
+  while (!Serial);
+
+
+  //Establishing the communication with the Motor Carrier
+  if (!controller.begin()){
     Serial.print("Motor Carrier connected, firmware version ");
     Serial.println(controller.getFWVersion());
   }
-  else
-  {
+  else {
     Serial.println("Couldn't connect! Is the red LED blinking? You may need to update the firmware with FWUpdater sketch");
     while (1);
   }
 
+
+  // Reboot the motor controller; brings every value back to default
+  Serial.println("reboot");
+  controller.reboot();
+  delay(500);
 }
 
 void loop() {
