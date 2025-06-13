461705a676046c06940aba86040db1f9daf42e10
notIsakS <notisaks@gmail.com>
2025-06-13 23:35:34 +0200

    Added camera and randomizer for blue cirlce and masking red area on robot

---
+++ src/camera.py
@@ -0,0 +1,55 @@
+# This program is designed to create a red circle
+# on the webcam display, and have a rover (blue circle)
+# create an optimal path to the red circle.
+# once the blue circle touches the red circle
+# a new red circle appears, and repeat.
+
+import cv2
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+import random as rand
+
+s = 0
+if len(sys.argv) > 1:
+    s = sys.argv[1]
+
+source = cv2.VideoCapture(s)
+win_name = 'Camera Preview'
+cv2.namedWindow(win_name, cv2.WINDOW_NORMAL)
+(x, y, windowWidth, windowHeight) = cv2.getWindowImageRect(win_name)
+
+
+def random_position():
+    return (rand.randint(0,620), rand.randint(0,460))
+
+rand_circle_pos = random_position()
+
+
+while cv2.waitKey(1) != 27: # Escape
+    has_frame, frame = source.read()
+    if not has_frame:
+        break
+
+    frame_with_circle = frame.copy()
+
+    cv2.circle(frame_with_circle, rand_circle_pos, 10, (255, 155, 0), 4)
+
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    lower_red = np.array([0, 155, 140])
+    upper_red = np.array([20, 255, 255])
+
+    mask = cv2.inRange(hsv, lower_red, upper_red)
+
+    result = cv2.bitwise_and(frame, frame, mask=mask)
+
+    cv2.imshow('with circle', frame_with_circle)
+    cv2.imshow('mask', result)
+    cv2.imshow(win_name, frame)
+
+print("Origin Coordinates(x,y): ", x, y)
+print("Width: ", windowWidth)
+print("Height: ", windowHeight)
+
+source.release()
+cv2.destroyWindow(win_name)
--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,20 @@
 #include <ArduinoMotorCarrier.h>
 #include <Arduino.h>
 
+void printStatus(float voltage, int count1, float vel1, int count2, float vel2, int duty);
+void forward(int speed);
+void backwards(int speed);
+void brake();
+void coast();
+
+const int M4_PWM1 = 5;  //  D5 -> MP6522 IN1
+const int M4_PWM2 = 4;  //  D4 -> MP6522 IN2
 
 // Statemachine
 enum State { 
   IDLE,
   MOVING
 };
-
 
 // Current state
 // Will be made possible to write "STOP", or "RUN"
@@ -24,9 +31,8 @@ void setup()
   Serial.begin(115200); 
   //while (!Serial);
 
-
   //Establishing the communication with the Motor Carrier
-  if (!controller.begin()){
+  if (controller.begin()){
     Serial.print("Motor Carrier connected, firmware version ");
     Serial.println(controller.getFWVersion());
   }
@@ -35,38 +41,146 @@ void setup()
     while (1);
   }
 
-
   // Reboot the motor controller; brings every value back to default
   Serial.println("reboot");
   controller.reboot();
   delay(500);
+  
+  // Reset the encoders internal counter to zero
+  Serial.println("rest counters");
+  encoder1.resetCounter(0);
+  encoder2.resetCounter(0);
 
-  M3.setDuty(0);
+  // PWM for motors 0 MIN, 100 MAX
+  M1.setDuty(0); // Negative for CW direction
+  M2.setDuty(0); // Positive for CW direction
+
+  pinMode(M4_PWM1, OUTPUT);
+  pinMode(M4_PWM2, OUTPUT);
 
   if (!PMIC.enableBoostMode()) {
     Serial.println("Error enabling Boost Mode");
   }
+
+  batteryVoltage = battery.getRaw()/236.0; //236 for Nano, 77 for MKR.
+  Serial.print("Battery voltage: ");
+  Serial.print(batteryVoltage, 3);
+  Serial.print("V, Raw ");
+  Serial.println(battery.getRaw());
 }
 
 void loop() {
-  batteryVoltage = battery.getRaw()/236.0; //236 for Nano, 77 for MKR.
-  Serial.print("Battery voltage: ");
-  Serial.print(batteryVoltage, 3);  
-  Serial.print("V, Raw ");
-  Serial.println(battery.getRaw());
-  delay(5000); //wait for a few seconds  
-  /* 
+/* 
   Goals:
   Communicate with Arduino IoT Nano 33 - Done
   Make the rover go in a straight line and circles - tbd
   Get the forklift working with servo motor - tbd
-  */
+*/
 
-  for (int i = 45; i < 120; i += 1){
-    servo3.setAngle(i);
-    Serial.print("Servos position");
-    Serial.println(i);
-    delay(20);
+  Serial.print("Encoder1 Pos [counts]: ");
+  Serial.print(encoder1.getRawCount());
+  Serial.print(" Encoder1 vel [count/sec]:");
+  Serial.println(encoder1.getCountPerSecond());
+  Serial.print("Encoder2 pos [counts]: ");
+  Serial.print(encoder2.getRawCount());
+  Serial.print(" Encoder2 vel [count/sec]:");
+  Serial.println(encoder2.getCountPerSecond());
+  Serial.println("");
+  delay(5000);
+
+  if (Serial.available() > 0){
+    int dutyCycle = Serial.parseInt();
+    int safeDuty = constrain(dutyCycle, 0, 100);
+
+    Serial.print("Received duty cycle: ");
+    Serial.println(safeDuty);
+    
+    M1.setDuty(-safeDuty);
+    M2.setDuty(safeDuty);
+
+    /*
+    // Controlling PWM signal by COM port
+    switch (dutyCycle) {
+      case 0:
+        M1.setDuty(-safeDuty);
+        M2.setDuty(safeDuty);
+      case 10:
+        M1.setDuty(-safeDuty);
+        M2.setDuty(safeDuty);
+      case 25:
+        M1.setDuty(-safeDuty);
+        M2.setDuty(safeDuty);
+      case 50:
+        M1.setDuty(-safeDuty);
+        M2.setDuty(safeDuty);
+      case 75:
+        M1.setDuty(-safeDuty);
+        M2.setDuty(safeDuty);
+      case 100:
+        M1.setDuty(-safeDuty);
+        M2.setDuty(safeDuty);
+      default:
+      Serial.println("Unknown value, try: 0, 10, 25, 50, 75, 100");
+    }
+    */
   }
+  
+  forward(128);   // 50% PWM
+  backwards(128); // 50% PWM
+  brake();        // Immediate stop
+  coast();        // Relaxed stop
+
   controller.ping();
+  delay(50);
+}
+
+
+void printStatus(float voltage, int count1, float vel1, int count2, float vel2, int duty){
+  Serial.write(27);
+  Serial.print("[2J");
+  Serial.write(27);
+  Serial.print("[H");
+
+  Serial.print("Battery Voltage: ");
+  Serial.print(voltage, 3);
+  Serial.println(" V");
+
+  Serial.print("Encoder1: ");
+  Serial.print(count1);
+  Serial.print(" counts | ");
+  Serial.print(vel1);
+  Serial.println(" counts/s");
+
+  Serial.print("Encoder2: ");
+  Serial.print(count2);
+  Serial.print(" counts | ");
+  Serial.print(vel2);
+  Serial.print(" counts/s");
+
+  Serial.print("Duty Cycle: ");
+  Serial.print(duty);
+  Serial.println(" %");
+}
+
+
+void forward(int speed) {
+  speed = constrain(speed, 0, 255);
+  digitalWrite(IN1, LOW);  // OUT1 Low
+  analogWrite(IN2, speed); // OUT2 High
+}
+
+void backwards(int speed) {
+  speed = constrain(speed, 0, 255);
+  digitalWrite(IN2, LOW);  // OUT1 LOW
+  analogWrite(IN1, speed); // OUT2 HIGH
+}
+
+void brake() {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, HIGH);
+}
+
+void coast() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
 }
